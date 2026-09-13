4a28632988abe9bf7dd18a0031fd2b16b395dee5
superdev0925 <superdev0925>
2026-09-13 03:34:50 +0300

    fix: correct metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://aliciamartin-portfolio.netlify.app/"),
+  metadataBase: new URL("https://mumei-2d-animator-portfolio.netlify.app/"),
   title: "Mumei",
   description: "Animator & Character Designer",
  
   authors: [{ name: "Mumei" }],
   openGraph: {
     title: "Animator & Character Designer",
-    url: "https://aliciamartin-portfolio.netlify.app/",
+    url: "https://mumei-2d-animator-portfolio.netlify.app/",
     siteName: "Mumei's Portfolio"
   },
 };
